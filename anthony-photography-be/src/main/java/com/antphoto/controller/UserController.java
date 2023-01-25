d28bc5d1c4f060d062732e7c5409133af5d3359d
Toacin <toacinp@outlook.com>
2023-01-25 13:58:59 -0700

    Users controller foundation complete

--- anthony-photography-be/src/main/java/com/antphoto/controller/UserController.java
+++ anthony-photography-be/src/main/java/com/antphoto/controller/UserController.java
@@ -1,9 +1,52 @@
 package com.antphoto.controller;
 
+import com.antphoto.model.User;
+import com.antphoto.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+    @Autowired
+    UserRepository repository;
 
+    @GetMapping("/api/users")
+    public List<User> getAllUsers() {
+        List<User> userList = repository.findAll();
+        return userList;
+    }
 
+    @GetMapping("/api/users/{id}")
+    public User getSingleUser(@PathVariable Integer id) {
+        User user = repository.getById(id);
+        return user;
+    }
+
+    @PostMapping("/api/users")
+    public User addUser(@RequestBody User user) {
+        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+        repository.save(user);
+        return user;
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        User tempUser = repository.getById(id);
+        if (!tempUser.equals(null)) {
+            user.setId(tempUser.getId());
+            repository.save(user);
+        }
+        return user;
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id) {
+        repository.deleteById(id);
+    }
 }
